25324cc380d45883bc00ad8a36dc8569cdbcf028
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-03 16:14:44 +0545

    React State

--- src/components/State/myState.js
+++ src/components/State/myState.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 export class Message extends Component {
+    constructor() {
+        super()
+        this.state = {
+            Message: 'Welcome Visitor'
+        }
+    }
     render() {
         return (
             <div className = "App">
-                <h1>Hello World</h1>
+                <h1>{this.state.Message}</h1>
             </div>
         );
     };
