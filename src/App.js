63f81491b68c348a2d072e09e63d17342f900d54
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-03 17:18:33 +0545

    setState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 // import People from './components/Props/myProps';
-import Message from './components/State/myState';
+// import Message from './components/State/myState';
+import Counter from './components/State/Counter';
 import './index.css';
 
 function App() {
     return (
         <div>
             {/* <People />  */}
-            <Message />
+            {/* <Message /> */}
+            <Counter />
         </div>
     );
 };
