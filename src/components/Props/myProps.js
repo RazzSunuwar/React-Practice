a0e004afa5e0cd78959c821f4c27c0397ea04439
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-03 13:48:37 +0545

    React Props

---
+++ src/components/Props/myProps.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+// CHILDREN PROPS
+
+function People(){
+    const friends = [
+        {name:"Linus Trovald", job:"Software Engineer, Linux Developer", age: "45", company : "Linux"}, 
+        {name:"Ryan Dahl", job:"Software Engineer", age : "39", company : "Google"},
+        {name:"Brad Traversy", job:"Web Developer", age : "39", company : "Freelancing, Udemy" },
+        {name:"Mosh", job:"React Developer", age : "39", company : "Youtube"},
+        {name:"Beau", job:"React Developer",age : "39", company : "freecodecamp"}
+    ]
+    return(
+        <section>
+            <Person person={friends[0]} />
+            <Person person={friends[1]} />
+            <Person person={friends[2]} />
+            <Person person={friends[3]} />
+            <Person person={friends[4]} />
+        </section>
+    )
+}
+
+const Person = props => {
+    const{name,job,age,company} = props.person;
+    const { children} = props;
+    console.log(props);
+    return(
+        <article>
+            <h1>{name}</h1>
+            {children}
+            <p>{job}</p>
+            <p>{age}</p>
+            <p>{company}</p>
+            <hr />
+        </article>
+    )
+}
+
+export default People;
+
+// Note :   
+// 1st parameter that we're passing argument was what we gonna be rendering
+// 2nd parameter is where is gonna be rendered.
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,46 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css'
-
-// CHILDREN PROPS
-
-function People(){
-    const friends = [
-        {name:"Linus Trovald", job:"Software Engineer, Linux Developer", age: "45", company : "Linux"}, 
-        {name:"Ryan Dahl", job:"Software Engineer", age : "39", company : "Google"},
-        {name:"Brad Traversy", job:"Web Developer", age : "39", company : "Freelancing, Udemy" },
-        {name:"Mosh", job:"React Developer", age : "39", company : "Youtube"},
-        {name:"Beau", job:"React Developer",age : "39", company : "freecodecamp"}
-    ]
-    return(
-        <section>
-            <Person person={friends[0]}>
-            <h1>Some heading</h1>
-            <p>Some info about</p>
-            </Person>
-            <Person person={friends[1]}/>
-            <Person person={friends[2]}/>
-            <Person person={friends[3]}/>
-            <Person person={friends[4]}/>
-        </section>
-    )
-}
-
-const Person = props => {
-    const{name,job,age,company} = props.person;
-    const { children} = props;
-    console.log(props);
-    return(
-        <article>
-            <h1>{name}</h1>
-            {children}
-            <p>{job}</p>
-            <p>{age}</p>
-            <p>{company}</p>
-            <hr />
-        </article>
-    )
-}
+import People from './components/Props/myProps';
+import './index.css';
 
 ReactDOM.render(<People />, document.getElementById("root"));
 
