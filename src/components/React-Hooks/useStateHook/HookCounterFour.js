fefdb02bdf0c65113e3daea8eff4850656fbbc28
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-11 23:14:10 +0545

    useState with Array

--- src/components/React-Hooks/useStateHook/HookCounterFour.js
+++ src/components/React-Hooks/useStateHook/HookCounterFour.js
@@ -4,8 +4,8 @@ function HookCounterFour() {
     const [items, setItems] = useState([]);
 
     const addItem = () => {
-        setItems([...items, {
-            id: items.length,
+        setItems([...items, {       //make copy using spread operator
+            id: items.length,       //append new item and pass it as argument to setItems function
             value: Math.floor(Math.random() * 10) + 1
         }])
     }
@@ -24,3 +24,11 @@ function HookCounterFour() {
 };
 
 export default HookCounterFour;
+
+/*Note:
+The useState hook lets you add state of functional components.
+In class, the state is always object.
+With the useState hook, the state doesn't have to be an object.
+The useState hook returns an array with 2 elements.
+The first element is the current value of the state, and the second element is a state
+setter function. */
