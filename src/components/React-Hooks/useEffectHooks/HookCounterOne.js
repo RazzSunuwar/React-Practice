a5bcd58d0a50e9b2cabe9296cc2d52cd26ce19db
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-13 20:57:33 +0545

    Conditinally run effects

--- src/components/React-Hooks/useEffectHooks/HookCounterOne.js
+++ src/components/React-Hooks/useEffectHooks/HookCounterOne.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 function HookCounterOne() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     useEffect(() => {
+        console.log('useEffect - Updating document title')
         document.title = `You clicked ${count} times`
-    })
+    }, [count]) /* Second parameter is the array of values that the effect depend on, 
+    if the value don't change between renders the effect is simply not run so good 
+    optimization technique to keep in mind */
 
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
             <button onClick= {() => setCount(count + 1)}>Click {count} times</button>
         </div>
     );
