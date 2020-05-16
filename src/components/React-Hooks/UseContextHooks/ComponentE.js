770c3480da847d35dd59a5e2b24a1eb76bc5f688
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-16 20:08:45 +0545

    useContext Hook

--- src/components/React-Hooks/UseContextHooks/ComponentE.js
+++ src/components/React-Hooks/UseContextHooks/ComponentE.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ComponentF from '../UseContextHooks/CompnentF';
+import { UserContext, ChannelContext } from '../../../App';
 
 function ComponentE() {
+    const user = useContext(UserContext)
+    const channel = useContext(ChannelContext)
     return (
         <div>
-            <ComponentF />
+            {/* <ComponentF /> */}
+            {user} - {channel}
         </div>
     );
 };
