cb8656fbe6d462139f4660f303ae42141077bcec
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-17 10:17:32 +0545

    useReducer (Complex state & action)

--- src/components/React-Hooks/useReducerHook/CounterTwo.js
+++ src/components/React-Hooks/useReducerHook/CounterTwo.js
@@ -1,15 +1,20 @@
 import React, { useReducer } from 'react';
 
 const intialState = {
-    firstCounter : 0
+    firstCounter : 0,
+    secondCounter : 10
 };
 
 const reducer = (state, action) => {
     switch(action.type) {
         case 'increment':
-            return {firstCounter: state.firstCounter + action.value}
+            return { ...state, firstCounter: state.firstCounter + action.value }
         case 'decrement':
-            return {firstCounter: state.firstCounter - action.value}
+            return { ...state, firstCounter: state.firstCounter - action.value }
+        case 'increment2':
+            return { ...state, firstCounter: state.firstCounter + action.value }
+        case 'decrement2':
+            return { ...state, firstCounter: state.firstCounter - action.value }
         case 'reset':
             return intialState
         default:
@@ -21,11 +26,14 @@ function CounterTwo() {
     const [count, dispatch] = useReducer(reducer, intialState)
     return (
         <div>
-            <div>Count - {count.firstCounter}</div>
+            <div> First Count - {count.firstCounter}</div>
+            <div> Second Count - {count.secondCounter}</div>
             <button onClick = {() => dispatch({type: 'increment', value: 1 })}>Increment</button>
             <button onClick = {() => dispatch({type: 'decrement', value: 1 })}>Decrement</button>
             <button onClick = {() => dispatch({type: 'increment', value: 5 })}>Increment 5</button>
             <button onClick = {() => dispatch({type: 'decrement', value: 5 })}>Decrement 5</button>
+            <button onClick = {() => dispatch({type: 'increment2', value: 2 })}>Increment Counter 2</button>
+            <button onClick = {() => dispatch({type: 'decrement2', value: 2 })}>Decrement Counter 2</button>
             <button onClick = {() => dispatch({type: 'reset'})}>Reset</button>
         </div>
     );
